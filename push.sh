750ad33d92a6b7f97b61d0e710444ea6d0f17565
Gary Williams <51086500+thisgeektweets@users.noreply.github.com>
2019-12-04 19:29:37 +0000

    Delete push.sh

--- push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
- docker push juanitomint/nextcloud-client
-# notify microbadger
-curl -X POST https://hooks.microbadger.com/images/juanitomint/nextcloud-client/_oZQ1x16Y_503mZGoIVtirP7sEs=
